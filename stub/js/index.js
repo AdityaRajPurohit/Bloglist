cb506d70c2e4c90263ba10e65871a45458eab105
Aditya Purohit <36314203+AdityaRajPurohit@users.noreply.github.com>
2019-06-23 22:29:39 +0530

    Add files via upload
    last commit

--- stub/js/index.js
+++ stub/js/index.js
@@ -1,5 +1,10 @@
+function getAllPosts() {
+    window.location.href= "./html/bloglist.html";
+}
 
-
+function openCreatePost() {
+    document.getElementById('newPostModal').style.display='block';
+}
 
 function onCloseNewPost() {
     document.getElementById('newPostModal').style.display='none';
